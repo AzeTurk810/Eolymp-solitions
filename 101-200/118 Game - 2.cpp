0fddeff14152dbb7ace5d552147db1ec2ade2d73
AzeTurk810 <167020195+AzeTurk810@users.noreply.github.com>
2025-03-01 10:51:30 +0400

    Create 118 Game - 2.cpp

---
+++ 101-200/118 Game - 2.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ull=unsigned long long;
+# define hurryup ios_base::sync_with_stdio(false);cin.tie();cout.tie()
+# define endl '\n'
+# define fori for(ll i=0; i<n; i++)
+# define forj for(ll j=0; j<m; j++)
+# define fors for(ll i=0; i<s.size(); i++)
+# define forv for(ll i=0; i<v.size(); i++)
+//# define mp make_pair
+# define pb push_back
+# define eb emplace_back
+# define ff first
+# define ss second
+# define sorta sort(a,a+n)
+# define sortv sort(v.begin(), v.end())
+# define sorts sort(s.begin(), s.end())
+# define mod 1000000007
+# define INF 1e9+7
+# define enterv while(cin>>i)v.pb(i);
+# define print(v) for(ll i = 0; i < v.size(); i++)cout<<v[i]<<" "
+map<ll,ll>mp;
+void quvvet(int n,map<ll,ll>&mp)
+{
+    
+    if(n>1)
+    {
+        for(int i=2;i<=n;i++)
+        {
+            if(n%i==0)
+            {if(mp[i]==0)mp[i]=1;else mp[i]++;quvvet(n,mp);break;}
+        }
+    }
+}
+// bool ikiquv(int n)
+// {
+//     while(n>1)
+//     {
+//         if(n%2!=0)
+//         return 0;
+//         n/=2;
+//     }
+//     return 1;
+// }
+// int ebob(int a,int b)
+// {
+//     return __gcd(a,b);
+// }
+// int ekob(int a,int b)
+// {
+//     return a*b/ebob(a,b);
+// }
+// bool palindromdur(string s)
+// {
+//     string s1=s;
+//     reverse(s1.begin(),s1.end());
+//     if(s1==s)
+//     return 1;
+//     return 0;
+// }
+int main()
+{
+    char k;
+    std::vector<int>v(4,0) ;
+    while(cin>>k)
+    {
+        if(k=='2')
+        v[2]++;
+        if(k=='3')
+        v[3]++;
+        if(k=='1')
+        v[1]++;
+    }
+    if((v[1]%2==0 && v[2]%2==0 && v[3]%2==0)||(v[1]%2==1 && v[2]%2==1 && v[3]%2==1))
+    cout<<0;
+    else
+    cout<<1;
+}
